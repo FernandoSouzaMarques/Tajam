0960a8082b0d293aed337711d6d4bf0c8051a99e
FernandoSouzaMarques <fernandinhosouzamarques@gmail.com>
2019-05-25 11:03:28 -0300

    adding background in top bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
-
 let btn = document.querySelector('[data-hamburger]');
-let menu = document.querySelector('[data-menu]');
 let spy = new Gumshoe('#my-awesome-nav a');
+const topBar = document.querySelector('.s-header__top-bar');
 
 btn.addEventListener('click', (e) => {
 	btn.classList.toggle('o-hamburger--is-active');
 	menu.classList.toggle('c-menu--is-active');
 })
+
+window.onscroll = scroll;
+
+function scroll () {
+	if (window.pageYOffset >= 20) {
+		topBar.classList.add('s-header__top-bar-bg');
+	} else {
+		topBar.classList.remove('s-header__top-bar-bg');
+	}
+}
