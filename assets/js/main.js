caaa9e76e6fce4f10f2f2dd2c8ef3b3deeaf4e18
FernandoSouzaMarques <fernandinhosouzamarques@gmail.com>
2019-05-30 23:07:31 -0300

    adding flickity CDN

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
+// import Menu from './components/menu';
+
+// Menu.create();
+
 let btn = document.querySelector('[data-hamburger]');
 let menu = document.querySelector('[data-menu]');
 const topBar = document.querySelector('.s-header__top-bar');
 let spy = new Gumshoe('#my-awesome-nav a');
 spy.setup();
+
 
 btn.addEventListener('click', (e) => {
 	btn.classList.toggle('o-hamburger--is-active');
@@ -18,4 +23,3 @@ function scroll () {
 		topBar.classList.remove('s-header__top-bar-bg');
 	}
 };
-
